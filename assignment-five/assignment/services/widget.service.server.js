b4452be39a32ecdf004ff65ee4adf5d5941dce7c
zilingyang <yang.zil@husky.neu.edu>
2019-01-17 20:01:13 -0800

    del samples

--- assignment-five/assignment/services/widget.service.server.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-  app.get("/api/widget", findAllWidgets);
-
-  var WIDGETS = require("./widget.mock.server.js");
-  function findAllWidgets(req, res) {
-    res.json(WIDGETS);
-  }
-}
--- assignment-five/src/app/pipes/to-upper-case.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToUpperCasePipe } from './to-upper-case.pipe';
-
-describe('ToUpperCasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToUpperCasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
