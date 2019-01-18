f37bd518f3a02cc6a829f3eba967afaf1d936ce0
zilingyang <yang.zil@husky.neu.edu>
2019-01-17 20:03:57 -0800

    del all

--- assignment-five/src/app/services/widget.mock.ts
+++
@@ -1,7 +0,0 @@
-import {Widget} from '../models/widget.model.client';
-
-export const WIDGETS: Widget[] = [
-  { "_id": "123", "type": "HEADING",  "size": 2, "text": "GIZMODO", "src": ""},
-  { "_id": "234", "type": "HEADING", "size": 4, "text": "Lorem ipsum", "src":""},
-  { "_id": "345", "type": "IMAGE", "size": 5, "text": "testing", "src": ""}
-];
