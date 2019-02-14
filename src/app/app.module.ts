546aee70f0eb9a2bec1bdb4f8ec60c7afdc0b5b0
zilingyang <yang.zil@husky.neu.edu>
2019-02-13 19:50:27 -0800

    Create angular components and their templates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,37 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './views/user/login/login.component';
 import { ProfileComponent } from './views/user/profile/profile.component';
 import { RegisterComponent } from './views/user/register/register.component';
+import { PageNewComponent } from './views/page/page-new/page-new.component';
+import { PageListComponent } from './views/page/page-list/page-list.component';
+import { PageEditComponent } from './views/page/page-edit/page-edit.component';
+import { WebsiteNewComponent } from './views/website/website-new/website-new.component';
+import { WebsiteEditComponent } from './views/website/website-edit/website-edit.component';
+import { WebsiteListComponent } from './views/website/website-list/website-list.component';
+import { WidgetChooserComponent } from './views/widget/widget-chooser/widget-chooser.component';
+import { WidgetEditComponent } from './views/widget/widget-edit/widget-edit.component';
+import { WidgetListComponent } from './views/widget/widget-list/widget-list.component';
+import { WidgetHeaderComponent } from './views/widget-edit/widget-header/widget-header.component';
+import { WidgetImageComponent } from './views/widget-edit/widget-image/widget-image.component';
+import { WidgetYoutubeComponent } from './views/widget-edit/widget-youtube/widget-youtube.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ProfileComponent,
-    RegisterComponent
+    RegisterComponent,
+    PageNewComponent,
+    PageListComponent,
+    PageEditComponent,
+    WebsiteNewComponent,
+    WebsiteEditComponent,
+    WebsiteListComponent,
+    WidgetChooserComponent,
+    WidgetEditComponent,
+    WidgetListComponent,
+    WidgetHeaderComponent,
+    WidgetImageComponent,
+    WidgetYoutubeComponent
   ],
   imports: [
     BrowserModule,
