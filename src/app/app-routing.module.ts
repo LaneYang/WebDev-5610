b54e9b9e434e83e8314ebec4bc8f0a35b99b942a
zilingyang <yang.zil@husky.neu.edu>
2019-02-14 19:24:09 -0800

    step1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import {LoginComponent} from './views/user/login/login.component';
 import {ProfileComponent} from './views/user/profile/profile.component';
 import {RegisterComponent} from './views/user/register/register.component';
 
-
 const routes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: 'profile', component: ProfileComponent},
-    {path: 'register', component: RegisterComponent}];
+  {path: 'login' , component :  LoginComponent},
+  {path: 'profile' ,  component: ProfileComponent} ,
+  {path : 'register' , component: RegisterComponent} ,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './views/user/login/login.component';
 import { ProfileComponent } from './views/user/profile/profile.component';
 import { RegisterComponent } from './views/user/register/register.component';
-import { PageNewComponent } from './views/page/page-new/page-new.component';
 import { PageListComponent } from './views/page/page-list/page-list.component';
+import { PageNewComponent } from './views/page/page-new/page-new.component';
 import { PageEditComponent } from './views/page/page-edit/page-edit.component';
-import { WebsiteNewComponent } from './views/website/website-new/website-new.component';
 import { WebsiteEditComponent } from './views/website/website-edit/website-edit.component';
 import { WebsiteListComponent } from './views/website/website-list/website-list.component';
+import { WebsiteNewComponent } from './views/website/website-new/website-new.component';
 import { WidgetChooserComponent } from './views/widget/widget-chooser/widget-chooser.component';
-import { WidgetEditComponent } from './views/widget/widget-edit/widget-edit.component';
 import { WidgetListComponent } from './views/widget/widget-list/widget-list.component';
-import { WidgetHeaderComponent } from './views/widget-edit/widget-header/widget-header.component';
-import { WidgetImageComponent } from './views/widget-edit/widget-image/widget-image.component';
-import { WidgetYoutubeComponent } from './views/widget-edit/widget-youtube/widget-youtube.component';
+import { WidgetYoutubeComponent } from './views/widget/widget-edit/widget-youtube/widget-youtube.component';
+import { WidgetImageComponent } from './views/widget/widget-edit/widget-image/widget-image.component';
+import { WidgetHeaderComponent } from './views/widget/widget-edit/widget-header/widget-header.component';
 
 @NgModule({
   declarations: [
@@ -25,18 +24,17 @@ import { WidgetYoutubeComponent } from './views/widget-edit/widget-youtube/widge
     LoginComponent,
     ProfileComponent,
     RegisterComponent,
-    PageNewComponent,
     PageListComponent,
+    PageNewComponent,
     PageEditComponent,
-    WebsiteNewComponent,
     WebsiteEditComponent,
     WebsiteListComponent,
+    WebsiteNewComponent,
     WidgetChooserComponent,
-    WidgetEditComponent,
     WidgetListComponent,
-    WidgetHeaderComponent,
+    WidgetYoutubeComponent,
     WidgetImageComponent,
-    WidgetYoutubeComponent
+    WidgetHeaderComponent
   ],
   imports: [
     BrowserModule,
